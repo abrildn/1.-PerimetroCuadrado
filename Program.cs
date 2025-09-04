51b77795e0f92e85211fde31691f9877213325de
Abril <abrudomingueznoya@gmail.com>
2025-09-04 15:27:03 -0300

    funciona

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PerimetroCuadrado 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lado, perimetro;
+            string linea;
+            Console.WriteLine("Longitud del lado del cuadrado:");
+            linea = Console.ReadLine();
+            lado=int.Parse(linea);
+
+            perimetro = lado * 4;
+
+            Console.WriteLine($"El perímetro del cuadrado es:");
+            Console.WriteLine(perimetro);
+            Console.ReadKey();
+        }
+    }
+}
